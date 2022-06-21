1f8c8bb0042c01b8ed6c01b28af1474bae9c6506
Jonathan Spada <102171264+jonspada@users.noreply.github.com>
2022-06-21 12:07:03 -0300

    exercicio com operadores matemáticos

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+ alert("Insira dois números")
+ let numberOne = prompt("Digite oprimeiro número: ")
+ let numberTwo = prompt("Digite o segundo número: ")
+ let soma = (Number(numberOne) + Number(numberTwo));
+ alert(`A soma dos dois números é: `+ soma)
+ let sub = (Number(numberOne) - Number(numberTwo));
+ alert(`A subtração dos dois números é: ` + sub)
+ let mult = (Number(numberOne) * Number(numberTwo));
+ alert(`A multiplicação dos dois números é: ` + mult)
+ let div = (Number(numberOne) / Number(numberTwo));
+ alert(`A divisão dos dois números é: ` + div)
+ let resto = (Number(numberOne) % Number(numberTwo));
+ alert(`O resto dos dois números é: ` + resto)
+
+ let pair = (Number(numberOne) + Number(numberTwo));
+
+ if(pair % 2 == 0){
+   alert(`A soma dos dois números é par`)
+   } else {
+     alert(`A soma dos dois números é impar`)
+   }
+
+   
+if(Number(numberOne) === Number(numberTwo)){
+  alert (`Os números são iguais`)
+
+} else{
+  alert (`Os números são diferentes`)
+
+}
+ 
+
+ 
